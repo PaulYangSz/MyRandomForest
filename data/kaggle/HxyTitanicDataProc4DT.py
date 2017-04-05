42c2322308949d74464357e8afd357a4ee2f2152
Paul Yang <shohokuooo@gmail.com>
2017-04-05 17:55:18 +0800

    Add data and process

---
+++ data/kaggle/HxyTitanicDataProc4DT.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+# 这个是从HanXiaoyang的Titanic.ipynb中提取出的数据预处理方法,不然用Java写太费劲了。
+
+import pandas as pd #数据分析
+import numpy as np #科学计算
+from pandas import Series,DataFrame
+
+data_train = pd.read_csv("Train.csv")
+
+#Show first line contents.
+data_train.columns
+
+#Show every fields's count.
+data_train.info()
+
+#Show mean mix max and so on...
+data_train.describe()
+
+
+#Show a plot
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+plt.subplot2grid((2,3),(0,0))             # 在一张大图里分列几个小图
+data_train.Survived.value_counts().plot(kind='bar')# plots a bar graph of those who surived vs those who did not. 
+plt.title(u"获救情况 (1为获救)") # puts a title on our graph
+plt.ylabel(u"人数")  
+
+plt.subplot2grid((2,3),(0,1))
+data_train.Pclass.value_counts().plot(kind="bar")
+plt.ylabel(u"人数")
+plt.title(u"乘客等级分布")
+
+plt.subplot2grid((2,3),(0,2))
+plt.scatter(data_train.Survived, data_train.Age)
+plt.ylabel(u"年龄")                         # sets the y axis lable
+plt.grid(b=True, which='major', axis='y') # formats the grid line style of our graphs
+plt.title(u"按年龄看获救分布 (1为获救)")
+
+
+plt.subplot2grid((2,3),(1,0), colspan=2)
+data_train.Age[data_train.Pclass == 1].plot(kind='kde')   # plots a kernel desnsity estimate of the subset of the 1st class passanges's age
+data_train.Age[data_train.Pclass == 2].plot(kind='kde')
+data_train.Age[data_train.Pclass == 3].plot(kind='kde')
+plt.xlabel(u"年龄")# plots an axis lable
+plt.ylabel(u"密度") 
+plt.title(u"各等级的乘客年龄分布")
+plt.legend((u'头等舱', u'2等舱',u'3等舱'),loc='best') # sets our legend for our graph.
+
+
+plt.subplot2grid((2,3),(1,2))
+data_train.Embarked.value_counts().plot(kind='bar')
+plt.title(u"各登船口岸上船人数")
+plt.ylabel(u"人数")  
+plt.show()
+
+
+
+
+
+#我们这里用scikit-learn中的RandomForest来拟合一下缺失的年龄数据
+from sklearn.ensemble import RandomForestRegressor
+ 
+### 使用 RandomForestClassifier 填补缺失的年龄属性
+def set_missing_ages(df):
+    
+    # 把已有的数值型特征取出来丢进Random Forest Regressor中
+    age_df = df[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
+
+    # 乘客分成已知年龄和未知年龄两部分
+    known_age = age_df[age_df.Age.notnull()].as_matrix()
+    unknown_age = age_df[age_df.Age.isnull()].as_matrix()
+
+    # y即目标年龄
+    y = known_age[:, 0]
+
+    # X即特征属性值
+    X = known_age[:, 1:]
+
+    # fit到RandomForestRegressor之中
+    rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
+    rfr.fit(X, y)
+    
+    # 用得到的模型进行未知年龄结果预测
+    predictedAges = rfr.predict(unknown_age[:, 1::])
+    
+    # 用得到的预测结果填补原缺失数据
+    df.loc[ (df.Age.isnull()), 'Age' ] = predictedAges 
+    
+    return df, rfr
+
+def set_Cabin_type(df):
+    df.loc[ (df.Cabin.notnull()), 'Cabin' ] = "Yes"
+    df.loc[ (df.Cabin.isnull()), 'Cabin' ] = "No"
+    return df
+
+def set_Embarked_type(df):
+    df.loc[ (df.Embarked.isnull()), 'Embarked' ] = "Noooo"
+    return df
+
+data_train, rfr = set_missing_ages(data_train)
+data_train = set_Cabin_type(data_train)
+data_train = set_Embarked_type(data_train)
+data_train.info()
+
+data_train.to_csv('gen_train_data1.csv', index=False)
+
+
+
+if 0:
+    # 因为逻辑回归建模时，需要输入的特征都是数值型特征
+    # 我们先对类目型的特征离散/因子化
+    # 以Cabin为例，原本一个属性维度，因为其取值可以是['yes','no']，而将其平展开为'Cabin_yes','Cabin_no'两个属性
+    # 原本Cabin取值为yes的，在此处的'Cabin_yes'下取值为1，在'Cabin_no'下取值为0
+    # 原本Cabin取值为no的，在此处的'Cabin_yes'下取值为0，在'Cabin_no'下取值为1
+    # 我们使用pandas的get_dummies来完成这个工作，并拼接在原来的data_train之上，如下所示
+    dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix= 'Cabin')
+    
+    dummies_Embarked = pd.get_dummies(data_train['Embarked'], prefix= 'Embarked')
+    
+    dummies_Sex = pd.get_dummies(data_train['Sex'], prefix= 'Sex')
+    
+    dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix= 'Pclass')
+    
+    df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+    df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+    df
+
+
+
+
+    # 接下来我们要接着做一些数据预处理的工作，比如scaling，将一些变化幅度较大的特征化到[-1,1]之内
+    # 这样可以加速logistic regression的收敛
+    import sklearn.preprocessing as preprocessing
+    scaler = preprocessing.StandardScaler()
+    age_scale_param = scaler.fit(df['Age'])
+    df['Age_scaled'] = scaler.fit_transform(df['Age'], age_scale_param)
+    fare_scale_param = scaler.fit(df['Fare'])
+    df['Fare_scaled'] = scaler.fit_transform(df['Fare'], fare_scale_param)
+    df
+
+
+
+    # 我们把需要的feature字段取出来生成csv格式
+    train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+    cols = train_df.columns.tolist()
+    cols = cols[1:] + cols[:1]
+    cols
+    train_df = train_df[cols]
+train_df.to_csv('gen_train_data.csv', index=False)
+
+
+
+
+#Begin to process test data
+data_test = pd.read_csv("test.csv")
+data_test.loc[ (data_test.Fare.isnull()), 'Fare' ] = 0
+# 接着我们对test_data做和train_data中一致的特征变换
+# 首先用同样的RandomForestRegressor模型填上丢失的年龄
+tmp_df = data_test[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
+null_age = tmp_df[data_test.Age.isnull()].as_matrix()
+# 根据特征属性X预测年龄并补上
+X = null_age[:, 1:]
+predictedAges = rfr.predict(X)
+data_test.loc[ (data_test.Age.isnull()), 'Age' ] = predictedAges
+
+data_test = set_Cabin_type(data_test)
+dummies_Cabin = pd.get_dummies(data_test['Cabin'], prefix= 'Cabin')
+dummies_Embarked = pd.get_dummies(data_test['Embarked'], prefix= 'Embarked')
+dummies_Sex = pd.get_dummies(data_test['Sex'], prefix= 'Sex')
+dummies_Pclass = pd.get_dummies(data_test['Pclass'], prefix= 'Pclass')
+
+
+df_test = pd.concat([data_test, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df_test.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+df_test['Age_scaled'] = scaler.fit_transform(df_test['Age'], age_scale_param)
+df_test['Fare_scaled'] = scaler.fit_transform(df_test['Fare'], fare_scale_param)
+df_test
+
+test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+test.to_csv('gen_test_data.csv', index=False)
