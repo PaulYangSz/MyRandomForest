7f905b28de74588dfab8f6271d538d7b4c6cddd4
Paul Yang <shohokuooo@gmail.com>
2017-04-06 17:05:53 +0800

    Add process train.csv python codes.

--- data/kaggle/HxyTitanicDataProc4DT.py
+++ data/kaggle/HxyTitanicDataProc4DT.py
@@ -99,66 +99,62 @@ def set_Cabin_type(df):
     return df
 
 def set_Embarked_type(df):
-    df.loc[ (df.Embarked.isnull()), 'Embarked' ] = "Noooo"
+    df.loc[ (df.Embarked.isnull()), 'Embarked' ] = "Null"
     return df
+
+def set_AgeRang_type(df):
+    if(df.loc[df.Age, 'Age'] < 10):
+        df.loc[df.Age, 'Age'] = 1
+    if(df.loc[df.Age, 'Age'] < 20):
+        df.loc[df.Age, 'Age'] = 2
+    if(df.loc[df.Age, 'Age'] < 30):
+        df.loc[df.Age, 'Age'] = 3
+    if(df.loc[df.Age, 'Age'] < 40):
+        df.loc[df.Age, 'Age'] = 4
+    if(df.loc[df.Age, 'Age'] < 50):
+        df.loc[df.Age, 'Age'] = 5
+    if(df.loc[df.Age, 'Age'] < 60):
+        df.loc[df.Age, 'Age'] = 6
+    if(df.loc[df.Age, 'Age'] < 90):
+        df.loc[df.Age, 'Age'] = 7
 
 data_train, rfr = set_missing_ages(data_train)
 data_train = set_Cabin_type(data_train)
 data_train = set_Embarked_type(data_train)
+#data_train = set_AgeRang_type(data_train)
 data_train.info()
 
-data_train.to_csv('gen_train_data1.csv', index=False)
 
 
+data_train = data_train.filter(regex='Survived|Age|SibSp|Parch|Fare|Cabin|Embarked|Sex|Pclass')
+cols = data_train.columns.tolist()
+cols = cols[1:] + cols[:1]
+cols
+data_train = data_train[cols]
+data_train.info()
+print "data_train.head()"
+print data_train.head()
+data_train.to_csv('gen_train_str_data.csv', index=False)
 
-if 0:
-    # 因为逻辑回归建模时，需要输入的特征都是数值型特征
-    # 我们先对类目型的特征离散/因子化
-    # 以Cabin为例，原本一个属性维度，因为其取值可以是['yes','no']，而将其平展开为'Cabin_yes','Cabin_no'两个属性
-    # 原本Cabin取值为yes的，在此处的'Cabin_yes'下取值为1，在'Cabin_no'下取值为0
-    # 原本Cabin取值为no的，在此处的'Cabin_yes'下取值为0，在'Cabin_no'下取值为1
-    # 我们使用pandas的get_dummies来完成这个工作，并拼接在原来的data_train之上，如下所示
-    dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix= 'Cabin')
-    
-    dummies_Embarked = pd.get_dummies(data_train['Embarked'], prefix= 'Embarked')
-    
-    dummies_Sex = pd.get_dummies(data_train['Sex'], prefix= 'Sex')
-    
-    dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix= 'Pclass')
-    
-    df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
-    df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
-    df
+age_count = data_train[u'Age'].value_counts()
+#print age_count
+ageFig = age_count.plot(kind='bar').get_figure()
+ageFig.savefig('age.png');
+print data_train[u'Age'].describe()
+#data_train.boxplot(column="Age")
 
-
-
-
-    # 接下来我们要接着做一些数据预处理的工作，比如scaling，将一些变化幅度较大的特征化到[-1,1]之内
-    # 这样可以加速logistic regression的收敛
-    import sklearn.preprocessing as preprocessing
-    scaler = preprocessing.StandardScaler()
-    age_scale_param = scaler.fit(df['Age'])
-    df['Age_scaled'] = scaler.fit_transform(df['Age'], age_scale_param)
-    fare_scale_param = scaler.fit(df['Fare'])
-    df['Fare_scaled'] = scaler.fit_transform(df['Fare'], fare_scale_param)
-    df
-
-
-
-    # 我们把需要的feature字段取出来生成csv格式
-    train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
-    cols = train_df.columns.tolist()
-    cols = cols[1:] + cols[:1]
-    cols
-    train_df = train_df[cols]
-train_df.to_csv('gen_train_data.csv', index=False)
+FarePic = data_train.boxplot(column="Fare")
+#FarePic.savefig('fare.png');
 
 
 
 
 #Begin to process test data
 data_test = pd.read_csv("test.csv")
+print "Original test.csv"
+data_test.info()
 data_test.loc[ (data_test.Fare.isnull()), 'Fare' ] = 0
+
 # 接着我们对test_data做和train_data中一致的特征变换
 # 首先用同样的RandomForestRegressor模型填上丢失的年龄
 tmp_df = data_test[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
@@ -168,18 +164,11 @@ X = null_age[:, 1:]
 predictedAges = rfr.predict(X)
 data_test.loc[ (data_test.Age.isnull()), 'Age' ] = predictedAges
 
+# 把Cabin数据按照yes, no分类赋值。
 data_test = set_Cabin_type(data_test)
-dummies_Cabin = pd.get_dummies(data_test['Cabin'], prefix= 'Cabin')
-dummies_Embarked = pd.get_dummies(data_test['Embarked'], prefix= 'Embarked')
-dummies_Sex = pd.get_dummies(data_test['Sex'], prefix= 'Sex')
-dummies_Pclass = pd.get_dummies(data_test['Pclass'], prefix= 'Pclass')
 
 
-df_test = pd.concat([data_test, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
-df_test.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
-df_test['Age_scaled'] = scaler.fit_transform(df_test['Age'], age_scale_param)
-df_test['Fare_scaled'] = scaler.fit_transform(df_test['Fare'], fare_scale_param)
-df_test
-
-test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
-test.to_csv('gen_test_data.csv', index=False)
+test = data_test.filter(regex='Age|SibSp|Parch|Fare|Cabin|Embarked|Sex|Pclass')
+print "After making-up the Fare, Age and Cabin"
+test.info()
+test.to_csv('gen_test_str_data.csv', index=False)
