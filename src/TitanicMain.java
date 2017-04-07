14cdeeee1636a1de3f03b143d5e828c61060b325
Paul Yang <shohokuooo@gmail.com>
2017-04-08 00:09:34 +0800

    Add 5th to predict training data self in TitanicMain.

--- src/TitanicMain.java
+++ src/TitanicMain.java
@@ -30,8 +30,8 @@ public class TitanicMain {
          * 3rd, select a division method, which means ID3 or C4.5 and so on.
          */
         @SuppressWarnings("unused")
-        ID3Algo id3 = new ID3Algo(0.0);
-        C4p5Algo c4p5 = new C4p5Algo(0.0);
+        ID3Algo id3 = new ID3Algo(0.14);
+        C4p5Algo c4p5 = new C4p5Algo(0.23);
         
         /**
          * 4th, construct a decision tree model, and train it.
@@ -39,6 +39,24 @@ public class TitanicMain {
         DecisionTreeModel deciTreeModel = null;
         deciTreeModel = new DecisionTreeModel(trainSet, "", c4p5);
         deciTreeModel.startTraining();
+        
+        /**
+         * 5th, predict the original training data.
+         */
+        int corrNum = 0;
+        double corrRate = 0.0;
+        for(int i = 0; i < trainSet.rowNum; i++) {
+            int tmpY = deciTreeModel.doPredict(trainSet.xRowDataList.get(i));
+            if(tmpY == trainSet.yDataList.get(i)) {
+                corrNum++;
+            }
+            else {
+                System.out.printf("Wrong predict=%d Y=%d, X(%d)\n", tmpY, trainSet.yDataList.get(i), i);
+                //System.out.println(trainSet.xRowDataList.get(i) + "");
+            }
+        }
+        corrRate = (double)corrNum / trainSet.rowNum;
+        System.out.println("Predict original training data, Rate: " + corrRate);
     }
 
 }
